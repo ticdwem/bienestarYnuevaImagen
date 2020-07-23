e0c023b1858eed7fe80ee3ac2c9f7d5012173691
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-22 19:28:37 -0500

    cambiamos configuracion de la conexion

--- config/db.php
+++ config/db.php
@@ -4,10 +4,10 @@
  * creamos la conexion a la base de datos en una clase estatica
  */
 class Database{
-    static function connect(){
-        $db = new mysqli('107.180.26.91', 'SaludBienestar', '!]2t#ya8}&p)', 'saludbienestar');
-        $db->query("SET NAMES 'utf8'");
-        return $db;
+    public static function connect(){
+        $conexion = new mysqli('107.180.26.91', 'SaludBienestar', '!]2t#ya8}&p)', 'saludbienestar');
+        $conexion->query("SET NAMES 'utf8'");
+        return $conexion;
     }
 }
 
