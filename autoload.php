97906a1c45801a50a4dfc8e3c627b2264b6ac392
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-27 10:53:14 -0500

    corregimos autoload

--- autoload.php
+++ autoload.php
@@ -1,7 +1,6 @@
 <?php
-
-function controller_autoload($classname){
+function controllers_autoload($classname){
     include 'controllers/'.$classname.'.php';
 }
 
-spl_autoload_register('controller_autoload');
+spl_autoload_register('controllers_autoload');
