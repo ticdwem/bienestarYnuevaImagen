afc45639c0b40ed480b5c802f9b3e473914017d7
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-07-31 12:24:12 -0500

    agregamos una nueva constante para acortar las url requeridas

--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,5 @@
 <?php
-define("base_url", "http://192.168.1.10/cbyni/"); // base url para llamadas absolutas de la url 
+define("base_url", "http://localhost/bienestarYnuevaImagen/"); // base url para llamadas absolutas de la url 
+define("base_server", $_SERVER['DOCUMENT_ROOT'].'/bienestarYnuevaImagen'); // base url para llamadas absolutas de la url 
 define("controller_default", "PacienteController"); // controlador por defecto
 define("action_default","index");
-
-
