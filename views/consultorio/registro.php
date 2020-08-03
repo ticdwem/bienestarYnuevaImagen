c8f101f69c2c7e735c168f9cfc20e99653f3acd4
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-03 11:34:40 -0500

    cremos el formulario de alta de consultorio

---
+++ views/consultorio/registro.php
@@ -0,0 +1,60 @@
+<div class="card border-0 shadow my-2">
+	<div class="card-body p-5">
+		<figure>    
+			<img src="<?=base_url?>assets/img/logo.png" class="text-center" alt="logo salud y bienestar">
+			<figcaption>
+				<h2 class="font-weight-light"><p>ALTA CONSULTORIO</p></h2> 
+			</figcaption> 
+		</figure>
+		<div style="height: auto">
+			<form id="registro" action="<?=base_url?>Consultorio/save" method="POST">
+            
+				<div class="form-row ">
+					<div class="form-group col-md-4">
+                        <label for="inpuEstado">Estado</label>
+                        <select class="form-control inpuEstado" id="inpuEstadoConsultorio" name="inpuEstadoConsultorio">
+                            <option value="">SELECCIONE UN ESTADO</option>
+                            <?php while ($estado = $nombreE->fetch_object()):?>							
+							<option value="<?= $estado->id?>"><?= $estado->estado?></option> 							
+						<?php endwhile; ?>
+						</select>
+					</div>
+					<div class="form-group col-md-4">
+                        <label for="inpuMunicipio">Municipio</label>
+					    <select class="form-control altaConsultorio" id="inpuMunicipio" name="inpuMunicipioConsultorio">
+                            <option value="">SELECCIONE UN MUNICIPIO</option>
+					    </select>
+					</div>
+					<div class="form-group col-md-4">
+						<label for="inpuCP">CP.</label>
+						<input type="text" class="form-control" id="inpuCPConsultorio" name="inpuCPConsultorio" >
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="form-group col-md-4">
+						<label for="inpuColonia">Colonia</label>
+						<input type="text" class="form-control" id="inpuColoniaConsultorio" name="inpuColoniaConsultorio" >
+					</div>
+					<div class="form-group col-md-4">
+						<label for="inpuCalle">Calle</label>
+						<input type="text" class="form-control" id="inpuCalleConsultorio" name="inpuCalleConsultorio" >
+					</div>
+					<div class="form-group col-md-4">
+						<label for="inpuNumCasa">Numero Consultorio</label>
+						<input type="text" class="form-control" id="inpuNumCasaConsultorio" name="inpuNumCasaConsultorio" >
+					</div>
+                </div>
+                <div class="form-row">
+					<div class="form-group col-md-4">
+						<label for="intputname">Nombre</label>
+						<input type="text" class="form-control" id="intputnameConsultorio" name="intputnameConsultorio" >
+					</div>
+				</div>
+				
+				<!-- <button class="btn btn-primary" type="submit">Submit form</button> -->
+				<input type="submit" id="btn-env" values="enviar" name="saveConsultorio">
+			</form>
+		</div>
+	</div>
+</div>
+
