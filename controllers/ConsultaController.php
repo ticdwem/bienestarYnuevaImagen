76d605b9a70eed1e4b190883349ad00c601a2d59
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-04 19:57:40 -0500

    cambiamos la calase pedido copntroller

--- controllers/ConsultaController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-class PedidoController{
-    /*este es una clase de prueba para saber que todo esta bien relacionado */
-    public function index(){
-        echo "controllador pedidos, acction index";
-    }
-}
