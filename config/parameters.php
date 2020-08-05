ba13596959b2b3881ad1a49bf7aa92f186f34e79
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-04 19:50:03 -0500

    cambiamos el controller default para que llegue al loguin en primera insancia

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
 define("base_url", "http://localhost/bienestarYnuevaImagen/"); // base url para llamadas absolutas de la url 
-define("controller_default", "PacienteController"); // controlador por defecto
+define("controller_default", "LogginController"); // controlador por defecto
 define("action_default","index");
