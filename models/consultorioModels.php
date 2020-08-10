b77e64c8a0aa00b42e54469ec75d8a5dff623def
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-10 14:25:13 -0500

    agregamos propiedades y metodos para hacer la actualizacion

--- models/consultorioModels.php
+++ models/consultorioModels.php
@@ -4,17 +4,38 @@ require_once $_SERVER['DOCUMENT_ROOT']."/bienestarYnuevaImagen/config/modeloBase
 
 class Consultorio extends ModeloBase{
 
+    private $idConsultorio;
     private $nombre;
     private $municipio;
     private $cp;
     private $colonia;
     private $calle;
     private $numero;
+    private $mesoCons;
     
     public function __construct() {
         parent::__construct();
     }
 
+    /**
+     * Get the value of idConsultorio
+     */ 
+    public function getIdConsultorio()
+    {
+        return $this->idConsultorio;
+    }
+
+    /**
+     * Set the value of idConsultorio
+     *
+     * @return  self
+     */ 
+    public function setIdConsultorio($idConsultorio)
+    {
+        $this->idConsultorio = $idConsultorio;
+
+        return $this;
+    }
     /**
      * Get the value of nombre
      */ 
@@ -134,6 +155,25 @@ class Consultorio extends ModeloBase{
 
         return $this;
     }
+    /**
+     * Get the value of mesoCons
+     */ 
+    public function getMesoCons()
+    {
+        return $this->mesoCons;
+    }
+
+    /**
+     * Set the value of mesoCons
+     *
+     * @return  self
+     */ 
+    public function setMesoCons($mesoCons)
+    {
+        $this->mesoCons = $mesoCons;
+
+        return $this;
+    }
 
     public function insertConsultorio(){
         $insert = "INSERT INTO consultorio
@@ -147,4 +187,28 @@ class Consultorio extends ModeloBase{
         }
         return $result;
     }
+
+    public function updaControl($campo){
+    $update = "UPDATE consultorio SET $campo = '{$this->getMesoCons()}' WHERE id_consultorio = '{$this->getIdConsultorio()}'";
+    $query = $this->db->query($update);
+    $upConMeso = false;
+    if($query){
+        $upConMeso = true;
+    }
+    return $upConMeso;
+    }
+
+    public function getMedicinaModels(){
+        $medicina = "SELECT cn.id_consultorio,cn.nombreConsultorio,cn.meso_Consultorio,cn.consentrado_Consultorio,cn.statusConsultorio 
+                     FROM consultorio cn
+                     WHERE cn.id_consultorio = '{$this->getIdConsultorio()}' AND cn.statusConsultorio = 1";
+        $qiery = $this->db->query($medicina);
+        if($qiery && $qiery->num_rows == 1){
+            $consultorio = $qiery->fetch_object();
+            return $consultorio;
+        }
+    }
+
+    
+
 }
