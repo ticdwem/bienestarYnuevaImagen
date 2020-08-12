ab61faf030a5878285e793fe8c14031b820812b3
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-12 17:51:39 -0500

    eliminamos temporalmente los consultorios para el loggin

--- controllers/LogginController.php
+++ controllers/LogginController.php
@@ -4,9 +4,17 @@ require_once $_SERVER['DOCUMENT_ROOT']."/bienestarYnuevaImagen/models/logginMode
 class LogginController{
     /*este es una clase de prueba para saber que todo esta bien relacionado */
     public function index(){
-        $loginConsultorio = new Login();
-        $consutorio = $loginConsultorio-> getAll('consultorio');
-
+        // $loginConsultorio = new Login();
+        // $consutorio = $loginConsultorio-> getAll('consultorio');
+        // $password = password_hash('123456789',PASSWORD_BCRYPT,['cost'=>4]);
+        // $pass = '$2y$04$oeeUk0Ngo3dQafoULp.hxODhg0RKuJ5BUBypfzua5.p';
+        // echo $password."<br>";
+        // $verify = password_verify('123456789',$password);
+        // if($verify){
+        //     echo 'correcto';
+        // }else{
+        //     echo 'salio algo mal';
+        // }
         require_once 'views/loggin/index.php';
     }
 }
