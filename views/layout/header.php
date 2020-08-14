b0c4483bf84caa459810cd83ace262b239a3def5
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-14 14:20:09 -0500

    agregamos la sisesion que muestra el nombre de docotr y el nombre del consultorio

--- views/layout/header.php
+++ views/layout/header.php
@@ -43,14 +43,11 @@
             <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- <a class="navbar-brand" href="index.html">CByNI</a> --> <img src="<?=base_url?>assets/img/logo.png" width="225px" height="60px" alt=""> 
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button><!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Buscar Paciente" aria-label="Search" aria-describedby="basic-addon2" />
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+            <div class="" id="headerBienestar">
+                <div class="input-group divconsul">
+                    <p class="consul"><?php if(isset($_SESSION['usuario'])){echo$_SESSION['usuario']['datos']->nombreConsultorio;} ?></p>
                 </div>
             </div>
-            </form>
         <!-- Navbar-->
                 <ul class="navbar-nav ml-auto ml-md-0">
                     <li class="nav-item dropdown">
@@ -60,7 +57,7 @@
                             <a class="dropdown-item" href="#">Herramientas</a>
                               <a class="dropdown-item" href="#">Activity Log</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="login.html">Salir</a>
+                            <a class="dropdown-item" href="<?=base_url?>Loggin/logout">Salir</a>
                         </div>
                     </li>
                 </ul>
