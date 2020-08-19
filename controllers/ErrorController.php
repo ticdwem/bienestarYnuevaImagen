c6c6a43ebabdcc884df26226fb1fb6fc18749538
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-19 15:54:56 -0500

    creamos una carperta para las pantallas de errores, movemos la pantalla de error404 de layouts a error, creo una pantalla para mostrar errores en un update

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -3,6 +3,10 @@
 class ErrorController{
     /*este es una clase de prueba para saber que todo esta bien relacionado */
     public function index(){
-        require_once 'views/layout/error404.php';
+        require_once 'views/error/error404.php';
+    }
+
+    public function update(){
+        require_once 'views/error/errorUpdate.php';
     }
 }
