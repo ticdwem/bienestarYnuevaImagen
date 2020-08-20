6040835d6566e11e2a0a158073f864f1e13734c2
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-20 11:07:15 -0500

    agregamos la session

--- views/consultorio/consultaNuevoIngreso.php
+++ views/consultorio/consultaNuevoIngreso.php
@@ -8,6 +8,10 @@
          <p class="name"><?php echo ucwords(SED::decryption($datos->nombreCliente).' '.SED::decryption($datos->apPatCliente).' '.SED::decryption($datos->apMatCliente))?></p>
         </div>
     </div>
+    <?php
+        $datosSEssion = '';
+     if(isset($_SESSION["ingreso"])){echo '<p class="alert alert-danger error" role="alert">'.$_SESSION['ingreso']["error"]."</p>";$datosSEssion = $_SESSION["ingreso"]['datos'];}
+     ?>
     <form action="<?=base_url?>Consulta/ingreso" method="POST" id="frmCobroEstatura" novalidate>
         <input type="hidden" name="idPaciente" value="<?=$datos->idCliente?>">
         <div class="form-row">
@@ -17,7 +21,7 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="cobro">Cobro</label>
-                <input type="number" class="form-control" id="cobro" name="txtCobro">
+                <input type="number" class="form-control" id="cobro" name="txtCobro" value="<?php if($datosSEssion != '') echo $datosSEssion['cobro']; ?>">
                 <div class="" id="alertaCobro">
                 </div>
             </div>
@@ -25,14 +29,14 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="txtNumber">Estatura</label>
-                <input type="number" class="form-control" id="txtEstatura" name="txtEstatura" value="<?=$datos->estaturaCliente?>">
+                <input type="number" class="form-control" id="txtEstatura" name="txtEstatura" value="<?php if($datosSEssion != ''){echo $datosSEssion['estatura'];}else{echo $datos->estaturaCliente;} ?>">
                 <div class="" id="alertaEstatura">
                 </div>
             </div>
         </div>
         <div class="form-group">
             <label for="observacion">Observaciones</label>
-            <textarea class="form-control" id="observacion" name="txtObs" rows="3"></textarea>
+            <textarea class="form-control" id="observacion" name="txtObs" rows="3"><?php if($datosSEssion != '') echo $datosSEssion['cobro']; ?></textarea>
             <div class="" id="alertaObs">
                 </div>
         </div>
