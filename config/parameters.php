a1a5daab6f64bca69f238afae132d208320e2751
mike121986 <38016591+mike121986@users.noreply.github.com>
2020-08-26 12:29:57 -0500

    condicionamos la session del consultorio

--- config/parameters.php
+++ config/parameters.php
@@ -2,4 +2,6 @@
 define("base_url", "http://localhost/bienestarYnuevaImagen/"); // base url para llamadas absolutas de la url 
 define("controller_default", "LogginController"); // controlador por defecto
 define("action_default","index");
+if(isset($_SESSION['usuario'])){
 define("Consultorio",$_SESSION['usuario']['consultorio']);
+}
